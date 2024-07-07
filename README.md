13361a860dcb4aedc34a0499c6c2ec7826bb80b9
Sah-ı Meran <97757827+Sahmeranorg@users.noreply.github.com>
2024-07-07 18:55:38 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+**BU TOOL DEATH ARCHİVİST TARAFINDAN**
+**YAPILMIŞTIR**
+![alt text](https://resmim.net/cdn/2024/03/20/fDQEDb.jpg)
+
+**İÇERİKLER**
+**PYTHON TOOL 1**
+**PYTHON BOT 2**
+**SMS BOMBER 3**
+**DEVELOPER 99**
+**KANAL 0**
+
+
+```
+git clone https://github.com/sahmeranorg/deatharchivist/
+cd deatharchivist
+chmod +x *
+python kurulum.py
+python deatharchivist.py
+```
+
+# DAHA FAZLASI İÇİN TELEGRAM KATIL
+[TELEGRAM](https://t.me/endlessarchivist)
---
+++ deatharchivist.py
@@ -0,0 +1,62 @@
+import os
+import requests
+import socket
+import random
+import webbrowser
+os.system('clear')
+
+import pyfiglet 
+a= pyfiglet.figlet_format("DeathArchivist", font = "slant"  ) 
+print(a)
+
+print('\033[0;31m         THE TOOL BY DEATH ARCHİVİST√')
+print("""\033[1;31m
+   _____________________________________________
+   |Telegram:https://t.me/EndlessArchivist         |
+   |Kanal:https://t.me/OBirSanalTanrisi     |
+   |___________________________________________|
+""")
+print('\033[0;35m   _________________________________________________')
+print("""\033[1;36m \033[3m
+  [1]=>  PYTHON TOOL        
+  [2]=>  PYTHON BOT
+  [3]=>  SMS BOMBER        
+  
+         [99]=> TELEGRAM       [0]=> KANAL""")
+print('\033[0;35m   _________________________________________________                        ')
+
+##phishing install
+
+oo = input('Seçimini Yap Keke:  ')
+
+if oo == '1':
+	print ("YÜKLENİYOR ")
+	os.system("cd $HOME")
+	os.system("pkg install git -y")
+	webbrowser.open("https://www.mediafire.com/folder/m4o4enc7ne1xk/Tools")
+	
+	##PHİSHİNG 2 İNSTALL
+elif oo == '2':
+	os.system("cd $HOME")
+	webbrowser.open("https://www.mediafire.com/folder/rqzpgaiagi72f/Bots")
+
+elif oo == '3':
+	os.system ("cd $HOME")
+	os.system ("apt update && apt upgrade -y")
+	os.system("pkg install python git -y")
+	os.system ("git clone https://github.com/raiderrXD/RedHack-sms/")
+	os.system ("cd yigitreal-camhack")
+	os.system ("python setup.py")
+	os.system (" python yigitreal-KAMERA.py")
+	
+elif oo == '99':
+	import webbrowser
+
+	webbrowser.open('https://t.me/endlessarchivist')
+	
+elif oo == '0':
+	import webbrowser
+
+	webbrowser.open('https://t.me/OBirSanalTanrisi')
+
+	
